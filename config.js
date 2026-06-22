05b5e05a5480be010e2a4333a3d92f570c13061f
mblvaa <mashkawell@gmail.com>
2026-06-22 10:37:58 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Данные проекта Supabase.
+// Вставьте сюда Project URL и Publishable key / anon key из Supabase.
+
+window.SYSTEM_CONFIG = {
+  SUPABASE_URL: 'https://viqqbooeivgqgktgdlbp.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_RQVv9XhCOprL3XVgU-zuaw_6hRFef9q'
+};
